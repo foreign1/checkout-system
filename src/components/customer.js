2c2d1e43ed447fcee5197122862887f1b1e7a498
unknown <Jenyojohn24@gmail.com>
2020-11-13 01:40:54 +1345

    define customer component

---
+++ src/components/customer.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class Customer extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAppleCountChange = this.handleAppleCountChange.bind(this);
+    this.handleOrangeChange = this.handleOrangeChange.bind(this);
+    this.handleCheckOut = this.handleCheckOut.bind(this);
+  }
+
+  state = {
+    Apple: this.props.cart.Apple,
+    Orange: this.props.cart.Orange,
+  };
+
+  handleAppleCountChange = (e) => {
+    this.props.handleAppleCountChange(e.target.value);
+  };
+
+  handleOrangeChange = (e) => {
+    this.props.handleOrangeChange(e.target.value);
+  };
+
+  handleCheckOut = () => {
+    this.props.checkout();
+  };
+  render() {
+    return (
+      <div className="customer">
+        <span className="containerLabel">Customer</span>
+        <ul>
+          <li>
+            <span>Apple - </span>
+            <input
+              type="number"
+              value={this.props.cart.Apple}
+              id="appleCount"
+              onChange={this.handleAppleCountChange}
+            />
+          </li>
+          <li>
+            <span>Orange - </span>
+            <input
+              type="number"
+              value={this.props.cart.Orange}
+              id="orangeCount"
+              onChange={this.handleOrangeChange}
+            />
+          </li>
+        </ul>
+        <div className="bill">Total = {this.props.bill}</div>
+        <button
+          title="checkout"
+          className="checkout"
+          onClick={this.handleCheckOut}
+        >
+          Checkout
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Customer;
